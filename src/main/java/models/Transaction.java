0955a14b8f006fbc658d32e75fbf5ea5a7441644
Mitja Horvat <mitja.horvat.92@gmail.com>
2018-02-28 00:05:54 +0100

    model Transactions added

--- src/main/java/models/Transaction.java
+++ src/main/java/models/Transaction.java
@@ -1,4 +1,40 @@
 package models;
 
 public class Transaction {
+    private String tid;
+    private float price;
+    private int date;
+
+    public Transaction(){
+
+    }
+    public Transaction(String tid, float price, int date){
+        this.tid = tid;
+        this.price = price;
+        this.date = date;
+    }
+
+    public String getTid(){
+        return this.tid;
+    }
+
+    public void setTid(String tid){
+        this.tid = tid;
+    }
+
+    public float getPrice (){
+        return this.price;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+    public int getDate(){
+        return this.date;
+    }
+    public void setDate(int date){
+        this.date = date;
+    }
+
+
 }
